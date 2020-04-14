17efab26a502a5b0ec476b9315de0a32a46a9327
Heshvipatel17 <heshvipatel17@gmail.com>
2020-04-14 01:28:46 +0100

    hw

--- src/test/java/uk/co/barclays/testsuite/TestCase.java
+++ src/test/java/uk/co/barclays/testsuite/TestCase.java
@@ -7,17 +7,17 @@ import uk.co.barclays.testbase.TestBase;
 public class TestCase extends TestBase {
     HomePage homepage = new HomePage();
 
-    @Test
+    @Test(priority = 0, groups = {"Sanity","Regression"})
     public void verifyUserShouldHoverOnBankLink() {
         homepage.mouseHoverOnBankDropDownLink();
     }
 
-    @Test
+    @Test(priority = 1, groups = {"Smoke","Regression"})
     public void verifyUserShouldMouseHoverAndClickOnBorrowDropDownLink() {
         homepage.mouseHoverAndClickOnBorrowDropDownLink();
     }
 
-    @Test
+    @Test(priority = 2, groups = {"Regression"})
     public void verifyUserShouldNavigateToLoginPage() {
         homepage.clickOnLoginLink();
     }
